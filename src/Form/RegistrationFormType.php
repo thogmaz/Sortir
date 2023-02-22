955cc25a0f5b4640c092f3a48b9515f80927aaec
thomas <thomas.fanouillere@hotmail.fr>
2023-02-22 13:51:35 +0100

    22/02

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -24,14 +24,6 @@ class RegistrationFormType extends AbstractType
             ->add('photoProfil')
             ->add('campus')
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
