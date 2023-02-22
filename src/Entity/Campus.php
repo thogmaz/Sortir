804599902eef21dcf76827854ea82ac82b722564
thomas <thomas.fanouillere@hotmail.fr>
2023-02-22 10:49:27 +0100

    22/02

--- src/Entity/Campus.php
+++ src/Entity/Campus.php
@@ -10,6 +10,9 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: CampusRepository::class)]
 class Campus
 {
+    public function __toString(){
+        return $this->nom;
+    }
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
